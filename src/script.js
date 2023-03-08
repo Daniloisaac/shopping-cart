272c2692caf6c02bb5ee311c864e4eb379f550c3
danilo isaac <daniloisaac942@gmail.com>
2023-03-07 21:39:18 -0300

    feat: adicionei novas funcionalidades

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
-const sectionContainer = document.querySelector('.container');
 const sectionItems = document.querySelector('.items');
-const divMessage = document.createElement('div');
+const divMenu = document.querySelector('.div_menu');
 const totalPrice = document.createElement('li');
+const pMessage = document.createElement('p');
 
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
@@ -17,40 +17,28 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ sku, name, image }) => {
+const createProductItemElement = ({ sku, name, image, price }) => {
   const section = document.createElement('section');
 
   section.className = 'item';
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__price', `R$${price}`));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
 };
 
-function message(param) {
-  if (param === true) {
-    divMessage.className = 'loading';
-    divMessage.innerText = 'carregando...';
-    sectionContainer.insertBefore(divMessage, sectionItems);
-  } else {
-    divMessage.remove();
-  }
-}
-
 const addChieldsSectionItems = async (product = 'computador') => {
-  message(true);
-
   const { results } = await (fetchProducts(product));
 
-  message(false);
-
-  results.forEach(({ id, title, thumbnail }) => {
+  results.forEach(({ id, title, thumbnail, price }) => {
     const addItemsInSection = createProductItemElement({ 
       sku: id,
       name: title,
       image: thumbnail,
+      price,
     }); 
     sectionItems.appendChild(addItemsInSection);
   });
@@ -61,27 +49,40 @@ function calculateCart() {
   const button = document.querySelector('.empty-cart');
     
   totalPrice.className = 'total-price';
-  const productsDescription = document.querySelectorAll('.li_cart_item');
-  
+  const productsDescription = document.querySelectorAll('.p_cart_item');
   let total = 0;
-   
   productsDescription.forEach((product) => { 
     const price = product.innerHTML.split('$', 2)[1]; 
     total += Number(price);
   });
-  
-  totalPrice.innerText = total;
+  totalPrice.innerText = `Total R$ ${total}`;
   (section.insertBefore(totalPrice, button));
+}
+
+function messageCartEmpty() {
+  const div = document.getElementsByClassName('cart__item');
+  const OlCartItems = document.querySelector('.cart__items');
+  const section = document.querySelector('.cart');
+  divMenu.innerText = div.length;
+  if (div.length === 0) {
+    pMessage.className = 'p_no_cart_item';
+    pMessage.innerText = 'O seu carrinho está vazio';
+    section.insertBefore(pMessage, OlCartItems);
+  } else {
+    pMessage.remove();
+  }  
 }
 
 const cartItemClickListener = (event) => {
   const div = document.getElementsByClassName('cart__item');
+  if (event.path[1].classList[0] !== 'cart__item') return '';
     (event.path[1].remove());
     calculateCart();
     const array = [...div];
     const array2 = [];
-
+    messageCartEmpty();
     array.forEach((item) => array2.push(item.innerHTML));
+    console.log(array);
     saveCartItems(JSON.stringify(array2));
 };
 
@@ -91,14 +92,14 @@ const createCartItemElement = ({ name, salePrice, thumbnail }) => {
   const p = document.createElement('p');
 
   div.className = 'cart__item';
-  p.className = 'li_cart_item';
-  li.innerHTML = `${name}`;
+  p.className = 'p_cart_item';
   p.innerHTML = `R$${salePrice}`;
+  li.className = 'li_cart__item';
+  li.innerHTML = `${name}`;
 
   div.appendChild(createProductImageElement(thumbnail));
-  div.appendChild(li);
   div.appendChild(p);
-
+  div.appendChild(li);
   div.addEventListener('click', (event) => {
     cartItemClickListener(event);
     calculateCart();
@@ -107,25 +108,25 @@ const createCartItemElement = ({ name, salePrice, thumbnail }) => {
 };
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-  
+
+const OlCartItems = document.querySelector('.cart__items');
 const addElementsOlCartItems = async (event) => {
-  const OlCartItems = document.querySelector('.cart__items');
   const item = event.target.parentNode;
   const listProduct = getSkuFromProductItem(item);
-  const { title, price, thumbnail } = await fetchItem(listProduct);   
-
-  OlCartItems.appendChild(createCartItemElement({ 
-    name: title, salePrice: price, thumbnail,
-  }));
-
+  const { title, price, thumbnail } = await fetchItem(listProduct); 
+  const createCart = createCartItemElement({ 
+    name: title, salePrice: price, thumbnail });
+    console.log(OlCartItems);
+  OlCartItems.appendChild(createCart);
+  messageCartEmpty();
   const saved = JSON.parse(getSavedCartItems()) || [];
-  const array = [...saved, li.innerHTML];
+  const array = [...saved, createCart.innerHTML];
   saveCartItems(JSON.stringify(array));
   calculateCart();
 };
    
 function addGetstore() {
-  const OlCartItems = document.querySelector('.cart__items');
+  // const OlCartItems = document.querySelector('.cart__items');
   const item = JSON.parse(getSavedCartItems()) || [];
 
   for (let i = 0; i < item.length; i += 1) {
@@ -162,23 +163,52 @@ async function buttonAdd() {
 function buttonClearAll() {
   const lis = document.getElementsByClassName('cart__item');
   const buttonClear = document.querySelector('.empty-cart');
-
+ 
     buttonClear.addEventListener('click', () => {
     Array.from(lis).forEach((clean) => clean.remove());
     localStorage.removeItem('cartItems');
+    messageCartEmpty();
     calculateCart();
   });
 }
 
 async function buttonSearch() {
+  const inputSearch = document.querySelector('.input_search');
   const Search = document.querySelector('.button_search');
+
   Search.addEventListener('click', async () => {
-  const inputSearch = document.querySelector('.input_search').value;
-  sectionItems.innerHTML = '';
-  await addChieldsSectionItems(inputSearch);
-  document.querySelector('.input_search').value = '';
-  await buttonAdd();
+    sectionItems.innerHTML = '';
+    await addChieldsSectionItems(inputSearch.value);
+    inputSearch.value = '';
+    await buttonAdd();
   });  
+
+  inputSearch.addEventListener('keypress', async (event) => {
+    if (event.key !== 'Enter') return '';
+    sectionItems.innerHTML = '';
+    await addChieldsSectionItems(inputSearch.value);
+    await buttonAdd();
+    inputSearch.value = '';
+  });  
+}
+
+function btnMenuClick() {
+  const btnMenu = document.querySelector('.btn_menu');
+  const head = document.querySelector('.container-cartTitle'); 
+  const section = document.querySelector('.cart');
+
+  btnMenu.addEventListener('click', () => {
+    console.log(section);
+   if (section.classList[1] === 'close') {
+     console.log(head.classList.remove('close'));
+     console.log(btnMenu.classList.remove('close'));
+     console.log(section.classList.remove('close'));  
+   } else {
+    console.log(head.classList.toggle('close'));
+    console.log(btnMenu.classList.toggle('close'));
+    console.log(section.classList.toggle('close'));  
+   }
+  });
 }
 
 window.onload = async () => {
@@ -186,7 +216,9 @@ window.onload = async () => {
   await addChieldsSectionItems();
   await buttonAdd();
   await buttonSearch();
+  btnMenuClick();
   addGetstore();
   removeDivCart();
   buttonClearAll();
+  messageCartEmpty();
 };
